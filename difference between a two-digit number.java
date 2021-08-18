42e50892e955d5fe107e09fd219256e6d47bcadc
prashanna-28 <68066087+prashanna-28@users.noreply.github.com>
2021-08-18 11:21:51 +0530

    Create difference between a two-digit number.java

---
+++ difference between a two-digit number.java
@@ -0,0 +1,17 @@
+/*The difference between a two-digit number and the number obtained by interchanging the
+positions of its digits is 36. Design an algorithm to find the difference between the two digits of
+that number.*/
+package wipro;
+import java.util.*;
+public class difference {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n;
+int d=36;
+n=d/9;
+System.out.println(n);
+}
+
+}
